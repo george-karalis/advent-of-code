4db6846bc7ee2e95f0f9aac203425192e12c67cd
george-karalis <george.karalis@l3harris.com>
2024-02-24 20:36:33 +0000

    DAY1 Calibrate Trebuchet based on day's 1 instructions

--- src/adventofcode/trebuchet_calibrations.py
+++ src/adventofcode/trebuchet_calibrations.py
@@ -5,30 +5,62 @@ Calculates and recovers with immaculate precision the exact calibration value.
 
 import argparse
 import logging
+import sys
 from pathlib import Path
-from typing import Generator
 
 logger = logging.getLogger(__spec__.name if __spec__ else __name__)
 
 
-def input_decipher_iterator(input_file_path: Path) -> Generator:
+def calibrate(input_file_path: Path) -> None:
     """
     ### Calculate each lines's first and last digit sum, from the txt file.
     ---
     """
-
+    calibration_value = 0
     with open(input_file_path) as FileObj:
         for line in FileObj:
-            print(line)
-            break
+            calibration_value += get_first_and_last_digit(line)
+
+    logger.info(f"Calibration value: {calibration_value}")
+
+
+def get_first_and_last_digit(line: str) -> int:
+    """
+    ### Returns the first and last digits for a line as a 2-digit int.
+    ---
+    """
+    digits_in_line = [digit for digit in line if digit.isdigit()]
+    if not len(digits_in_line):
+        raise KeyError(f"No digit found in line: {line}")
+
+    return int(digits_in_line[0] + digits_in_line[-1])
 
 
 # TODO: find out how rich could work for the cli.
-def parse_arguments(argv: list[str]) -> argparse.Namespace:
+def parse_arguments(argv: list[str] | None = None) -> argparse.Namespace:
     """
-    Parse arguments via argv
+    ### Parse arguments via argv.
+    ---
     """
+    parser = argparse.ArgumentParser(
+        allow_abbrev=False,
+        description=__doc__,
+    )
+    parser.add_argument(
+        "-in", "--input-file", type=Path, help="Add the input file path."
+    )
+    return parser.parse_args(argv)
 
 
-def main(args):
-    input_decipher_iterator()
+def main(argv: list[str] | None = None) -> None:
+    args = parse_arguments(argv)
+
+    streamHandler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(streamHandler)
+    logger.setLevel(logging.INFO)
+
+    calibrate(args.input_file)
+
+
+if __name__ == "__main__":
+    main()
