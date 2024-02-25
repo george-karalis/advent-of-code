d08a6e2154bc74437dac3ed4bcabe3ff9b510809
george-karalis <george.karalis@l3harris.com>
2024-02-25 11:39:20 +0000

    DAY2 WIP: DAY2 setup Rebase

--- src/adventofcode/data_collection.py
+++ src/adventofcode/data_collection.py
@@ -2,6 +2,8 @@
 Contains all the necessary hard coded data, that each puzzle requires in order to reach to its resolution.
 """
 
+from dataclasses import dataclass
+
 # Day1: Collections of strings to be identify while scanning the calibration document
 
 NUMERICAL_DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -27,3 +29,19 @@ ALPHA_DIGITS = [
     "nine",
     "enin",
 ]
+
+# Day2: Satchel contents
+
+
+@dataclass(frozen=True)
+class Satchel:
+    red_count: int
+    green_count: int
+    blue_count: int
+
+
+SATCHEL = Satchel(
+    red_count=12,
+    green_count=13,
+    blue_count=14,
+)
